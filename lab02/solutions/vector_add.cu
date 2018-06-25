f9d770dabab2ac3d9051768a5207a6683efaeed4
Putt Sakdhnagool <15830559+puttsk@users.noreply.github.com>
2018-06-25 09:58:55 +0700

    Update comments

--- lab02/solutions/vector_add.cu
+++ lab02/solutions/vector_add.cu
@@ -1,19 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/time.h>
 #include <assert.h>
 #include <cuda.h>
 #include <cuda_runtime.h>
 
 #define N 10000000
 #define MAX_ERR 1e-6
-
-double gettime(){
-    struct timeval t;
-    gettimeofday(&t, 0);
-    return (t.tv_sec * 1000000 + t.tv_usec);
-}
 
 __global__ void vector_add(float *out, float *a, float *b, int n) {
     for(int i = 0; i < n; i ++){
@@ -25,10 +18,12 @@ int main(){
     float *a, *b, *out;
     float *d_a, *d_b, *d_out; 
 
+    // Allocate host memory
     a   = (float*)malloc(sizeof(float) * N);
     b   = (float*)malloc(sizeof(float) * N);
     out = (float*)malloc(sizeof(float) * N);
 
+    // Initialize host arrays
     for(int i = 0; i < N; i++){
         a[i] = 1.0f;
         b[i] = 2.0f;
@@ -43,14 +38,10 @@ int main(){
     cudaMemcpy(d_a, a, sizeof(float) * N, cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, sizeof(float) * N, cudaMemcpyHostToDevice);
 
-    // Timing execution time 
-    double timer = -gettime();
-    
+    // Executing kernel 
     vector_add<<<1,1>>>(d_out, d_a, d_b, N);
     
-    cudaDeviceSynchronize();
-    timer += gettime();
-
+    // Transfer data back to host memory
     cudaMemcpy(out, d_out, sizeof(float) * N, cudaMemcpyDeviceToHost);
 
     // Verification
@@ -58,12 +49,14 @@ int main(){
         assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
     }
 
-    printf("PASSED: %f s\n", timer / 1e6);
+    printf("PASSED\n");
 
+    // Deallocate device memory
     cudaFree(d_a);
     cudaFree(d_b);
     cudaFree(d_out);
 
+    // Deallocate host memory
     free(a); 
     free(b); 
     free(out);
--- lab02/vector_add.c
+++ lab02/vector_add.c
@@ -1,17 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <sys/time.h>
 #include <assert.h>
 
-#define N (1<<20)
+#define N 10000000
 #define MAX_ERR 1e-6
-
-double gettime(){
-    struct timeval t;
-    gettimeofday(&t, 0);
-    return (t.tv_sec * 1000000 + t.tv_usec);
-}
 
 void vector_add(float *out, float *a, float *b, int n) {
     for(int i = 0; i < n; i++){
@@ -22,24 +15,24 @@ void vector_add(float *out, float *a, float *b, int n) {
 int main(){
     float *a, *b, *out; 
 
+    // Allocate memory
     a   = (float*)malloc(sizeof(float) * N);
     b   = (float*)malloc(sizeof(float) * N);
     out = (float*)malloc(sizeof(float) * N);
 
+    // Initialize array
     for(int i = 0; i < N; i++){
         a[i] = 1.0f;
         b[i] = 2.0f;
     }
 
-    // profiling 
-    double timer = -gettime();
+    // Main function
     vector_add(out, a, b, N);
-    timer += gettime();
 
     // Verification
     for(int i = 0; i < N; i++){
         assert(fabs(out[i] - a[i] - b[i]) < MAX_ERR);
     }
 
-    printf("PASSED: %f ms\n", timer / 1e3);
+    printf("PASSED\n");
 }
